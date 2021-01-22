35eb470836eb88c2dbba49aad4a4fa96d2073edb
Amalina Aziz <amalina.aziz@free2move.se>
2021-01-22 22:03:17 +0800

    - cleanup code

--- products.js
+++ products.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-function Product(props) {
-    console.log(props)
+function Product (props) {
   return (
     <div>
-        Name: {props.product.name} <br />
+      Name: {props.product.name} <br />
         Description: {props.product.description} <br />
         Price: {props.product.price} <br /><br />
     </div>
