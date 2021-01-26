172df2716767200076962340f2785c4f62a3aa5f
Amalina Aziz <amalina.aziz@free2move.se>
2021-01-26 23:19:19 +0800

    - change header image url

--- Header.js
+++ Header.js
@@ -4,8 +4,8 @@ function Header() {
     return (
         <header>
             <img 
-                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
-                alt="Problem?"
+                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FTrollface&psig=AOvVaw05EAUxzLwIlVG3f-Jf02pi&ust=1611760709153000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMj-59fyue4CFQAAAAAdAAAAABAD" 
+                alt="Meme Generator"
             />
             <p>Meme Generator</p>
         </header>
